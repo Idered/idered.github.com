64c1b68c3634ecc764190197c99447b6e460b625
Kasper Mikiewicz <IderedPL@gmail.com>
2013-03-05 02:03:02 +0100

    add _site

---
+++ _push.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+git add .
+git commit -m "add _site"
+
+git update-ref refs/heads/master $(echo 'Add commit message here!' | git commit-tree dev^{tree}:_site -p $(cat .git/refs/heads/master))
+
+git checkout master
+
+git push
+
+exit 0
