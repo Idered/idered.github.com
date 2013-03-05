ff73563cd7301787d2a8c552f47b617feaf65ae9
Kasper Mikiewicz <IderedPL@gmail.com>
2013-03-05 02:08:46 +0100

    update site

--- _push.sh
+++ _push.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 git add .
+
 git commit -m "add _site"
 
 git update-ref refs/heads/master $(echo 'Add commit message here!' | git commit-tree dev^{tree}:_site -p $(cat .git/refs/heads/master))
@@ -8,5 +9,3 @@ git update-ref refs/heads/master $(echo 'Add commit message here!' | git commit-
 git checkout master
 
 git push origin master
-
-exit 0
